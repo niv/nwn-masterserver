59e1f0eb4262dc19160af394ed75cb7a50c19490
Bernhard Stoeckner <elven@swordcoast.net>
2011-07-12 11:51:42 +0200

    basic SQL-based authentication demo

--- mode_sql.rb
+++ mode_sql.rb
@@ -1,5 +1,34 @@
 require 'sequel'
+require 'digest/md5'
+
+DB = Sequel.sqlite("nwmaster.db")
 
 class IAuth
-  @db = Sequel.sqlite("nwmaster.db")
+  class Key < Sequel::Model
+  end
+  class Account < Sequel::Model
+  end
+
+  def verify_key publickey, hash
+    key = Key[publickey]
+    key or return [false, 0]
+
+    [true, key.expansion]
+  end
+
+  def authenticate account, salt, hash, platform
+    acc = Account[account]
+    acc or return false
+
+    # md5(md5(player password) + Salt)
+    hash == Digest::MD5.hexdigest(
+        Digest::MD5.hexdigest(acc.password) + salt)
+  end
+
+  def get_motd account
+    nil
+  end
+
+private
+
 end
---
+++ mode_sql.sql
@@ -0,0 +1,9 @@
+create table accounts (
+	account varchar primary key,
+	password varchar
+);
+
+create table keys (
+	key varchar primary key,
+	expansion int
+);
